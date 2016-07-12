b01ec5fd330559157e687bfcfa5aefffe45d8831
Matt Smollinger <matt.smollinger@mapzen.com>
2016-07-12 09:37:46 -0400

    Testing initial tangram import. Will refactor to utilize cocoapods once that's in place for tangram.

--- ios-sdk/ViewController.swift
+++ ios-sdk/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Pelias
 
-class ViewController: UIViewController, APIConfigData {
+class FirstViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -35,4 +35,29 @@ class ViewController: UIViewController, APIConfigData {
 
 
 }
+
+//class SecondViewController: ViewController {
+//  override func viewDidLoad() {
+//    super.viewDidLoad()
+//    // Do any additional setup after loading the view, typically from a nib.
+//  }
+//  
+//  override func viewDidAppear(animated: Bool) {
+//    super.viewDidAppear(animated)
+//    
+//    PeliasSearchManager.sharedInstance.apiKey = "search-tZu4e8k"
+//    
+//    let searchText = "Testing 1 2 3 4"
+//    let searchConfig = PeliasSearchConfig(searchText: searchText, completionHandler: { (searchResponse) -> Void in
+//      print(searchResponse)
+//    })
+//    PeliasSearchManager.sharedInstance.performSearch(searchConfig)
+//  }
+//  
+//  override func didReceiveMemoryWarning() {
+//    super.didReceiveMemoryWarning()
+//    // Dispose of any resources that can be recreated.
+//  }
+//
+//}
 
---
+++ ios-sdk/src/MapzenSearchManager.swift
@@ -0,0 +1,19 @@
+//
+//  MapzenSearchManager.swift
+//  ios-sdk
+//
+//  Created by Matt Smollinger on 3/15/16.
+//  Copyright © 2016 Mapzen. All rights reserved.
+//
+
+import Foundation
+import Pelias
+
+public final class MapzenSearchManager {
+  public static let sharedInstance = MapzenSearchManager()
+//  private let peliasSearch = PeliasSearchManager()
+  public var apiKey: String?
+
+  
+  
+}
